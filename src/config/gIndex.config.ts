c95d498b01754ec8732cb0989c5d3787b8e72670
Sussy Baka <95069650+lmao-ded1@users.noreply.github.com>
2026-05-21 15:29:30 +0530

    Update gIndex.config.ts

--- src/config/gIndex.config.ts
+++ src/config/gIndex.config.ts
@@ -124,7 +124,7 @@ const config: z.input<typeof Schema_Config> = {
      *
      * Default: 100MB
      */
-    streamMaxSize: 104857600,
+    streamMaxSize: 536870912,
 
     /**
      * Maximum file size that can be downloaded via api routes
@@ -137,7 +137,7 @@ const config: z.input<typeof Schema_Config> = {
      *
      * Default: 4MB
      */
-    maxFileSize: 4194304,
+    maxFileSize: 1073741824,
 
     /**
      * Allow user to download protected file without password.
